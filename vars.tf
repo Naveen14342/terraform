e20cd48e41b716eb3fbde8bc6d22eec2cb79784e
Naveen14342 <naveentharapatla@gmail.com>
2025-11-28 15:50:54 +0530

    Remove 'image' variable from vars.tf
    Removed the 'image' variable which defined the AMI for the instance.

--- vars.tf
+++ vars.tf
@@ -2,10 +2,7 @@ variable "giant" {
   description = "Instance region"
 }
 
-variable "image" {
-  description = "AMI for my instance"
-  default     = "ami-04eeb425707fa843c"
-}
+
 
 variable "size" {
   description = "Size of my instance"
